c2a7dc085985e4b09a492d6af52f76ec96507e31
Marat Khusnetdinov <marat@khusnetdinov.ru>
2023-06-01 22:03:08 +0300

    Lesson 1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use std::vec::Vec;
+
 fn main() {
-    println!("Hello, world!");
+    const COUNT: usize = 100;
+
+    let mut vector = Vec::with_capacity(COUNT);
+
+    for index in 1..COUNT {
+        let result = match (index % 3, index % 5) {
+            (0, 0) => String::from("FizzBuzz"),
+            (0, _) => String::from("Fizz"),
+            (_, 0) => String::from("Buzz"),
+            (_, _) => format!("{}", index),
+        };
+
+        vector.push(result);
+    }
+
+    print_result(&vector);
+}
+
+fn print_result(collection: &[String]) {
+    for index in collection {
+        println!("{}", index)
+    }
 }
